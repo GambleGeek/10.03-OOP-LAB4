5e9511a32f8148f5e972d70aa94d88e5173cf23e
Adis <75252443+GambleGeek@users.noreply.github.com>
2022-03-11 00:03:01 +0600

    Add files via upload

---
+++ lab4-oop-solo/src/air/DAO/DAO.java
@@ -0,0 +1,34 @@
+package air.DAO;
+
+import air.entity.*;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public interface DAO<T> {
+
+    public T get(int id);
+    public void getAll();
+    public void save(T object);
+    public void edit(int id, T object);
+    public void delete(int id);
+    public void editByName(String name, T object);
+    public void editByClientNumber(int client_number, T object);
+
+    static SessionFactory buildSessionFactory(){
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Client.class)
+                .addAnnotatedClass(Contract.class)
+                .addAnnotatedClass(Tour.class)
+                .addAnnotatedClass(TourType.class)
+                .addAnnotatedClass(Country.class)
+                .addAnnotatedClass(Program.class)
+                .addAnnotatedClass(TourTask.class)
+                .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Position.class)
+
+                .buildSessionFactory();
+        return factory;
+    }
+}
